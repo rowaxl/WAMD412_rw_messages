cdaa1fe60aa9be63472452e6b1adc1aff40891b8
rowaxl <rowaxl0@gmail.com>
2021-01-06 13:06:38 +0900

    split to router module

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
 const http = require('http')
-const index = require('./routers/index')
-const writeMessage = require('./routers/writeMessage')
-const readMessages = require('./routers/readMessages')
+const router = require('./router')
 
-const server = http.createServer((req, res) => {
-  console.log(req)
-
-  switch (req.url) {
-    case '/':
-      return index(req, res)
-    case '/write-message':
-      return writeMessage(req, res)
-    case '/read-message':
-      return readMessages(req, res)
-    default:
-      return index(req, res)
-  }
-})
+const server = http.createServer(router)
 
 server.listen(3000)
---
+++ router.js
@@ -0,0 +1,20 @@
+const index = require('./paths/index')
+const writeMessage = require('./paths/writeMessage')
+const readMessages = require('./paths/readMessages')
+
+const router = (req, res) => {
+  console.log(req)
+
+  switch (req.url) {
+    case '/':
+      return index(req, res)
+    case '/write-message':
+      return writeMessage(req, res)
+    case '/read-message':
+      return readMessages(req, res)
+    default:
+      return index(req, res)
+  }
+}
+
+module.exports = router
